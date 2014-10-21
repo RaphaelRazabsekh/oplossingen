4d7b5ea84dd00b7a22c165e750212f6610a81fe9
Raphaël Razab-Sekh <razab-sekh_1995@hotmail.com>
2014-10-21 10:51:49 +0200

    opdracht toegevoegd

---
+++ opdracht-comments/opdracht-comments.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Opdracht Comments</title>
+	</head>
+	<body>
+		<?php
+			echo "Raphaël Razab-Sekh";
+			#Raphaël Razab-Sekh
+			#iverick.razabsekh@student.kdg.be
+			#razab-sekh_1995@hotmail.com
+		?>
+	</body>
+</html>
